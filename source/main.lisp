6269107c9678d607d73d109678d62150e275de16
Lukáš Hozda <luk.hozda@gmail.com>
2024-09-03 09:00:00 +0200

    lisp bootstrapping: add source main module

---
+++ source/main.lisp
@@ -0,0 +1,33 @@
+(in-package #:world-peace)
+
+;;;; -- Command Line --
+
+(defun usage (&optional (stream *standard-output*))
+  "Print command-line usage to STREAM."
+  (format stream "usage: peace [repl | compile SOURCE-ROOT ENTRYPOINT -o OUTPUT]~%"))
+
+(defun main ()
+  "Run the World Peace command-line entry point."
+  (let ((arguments (uiop:command-line-arguments)))
+    (cond
+      ((or (null arguments)
+           (string= (first arguments) "repl"))
+       (run-repl))
+      ((string= (first arguments) "compile")
+       (destructuring-bind (&optional source-root entrypoint option output &rest extra)
+           (rest arguments)
+         (if (and source-root
+                  entrypoint
+                  option
+                  output
+                  (string= option "-o")
+                  (null extra))
+             (run-compiler :source-root source-root
+                           :entrypoint entrypoint
+                           :output output)
+             (progn
+               (usage *error-output*)
+               (uiop:quit 64)))))
+      (t
+       (usage *error-output*)
+       (uiop:quit 64)))))
